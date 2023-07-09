4c4a314d78c0338b1ede41a7f23554dba70a08f2
Rakan1837 <hamouri.rakan@gmail.com>
2023-07-09 00:57:58 -0400

    modified scrape.py to take in email and password inputs for instacart login

--- scrape.py
+++ scrape.py
@@ -17,6 +17,8 @@ def IC_Scraper(query, path):
     This is hard coded for instacart website for now, could be modified later 
     Second argument is the path of the chromedriver on your device
     """
+    email = "yuyuwrizz123@gmail.com"
+    passw = 23423
     options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     options.add_experimental_option('excludeSwitches',['enable-logging'])
@@ -25,13 +27,19 @@ def IC_Scraper(query, path):
     driver.implicitly_wait(5)
     driver.get("https://www.instacart.ca/store")
     # query = "Walmart"
-    search_input = driver.find_element(By.ID, "search-bar-input")
+    # search_input = driver.find_element(By.ID, "search-bar-input")
+    email_input = driver.find_element(By.NAME,"email")
+    email_input.send_keys(email)
+    driver.implicitly_wait(3)
+    pass_input = driver.find_element(By.NAME, "password")
+    pass_input.send_keys(passw)
+    # Checkpoint: currently ran into an issue where instacart does not let bot browser through
+    # shops unless they're logged in, the above code inputs email and password into the fields, (tested & works)
+    # search_input.send_keys(query)
 
-    search_input.send_keys(query)
-
-    action = ActionChains(driver)
-    action.send_keys(Keys.RETURN)
-    action.perform()
+    # action = ActionChains(driver)
+    # action.send_keys(Keys.RETURN)
+    # action.perform()
     print("tests passed")  
     #driver.quit()
 # Example Call Below, will move this to main when details are finalized
